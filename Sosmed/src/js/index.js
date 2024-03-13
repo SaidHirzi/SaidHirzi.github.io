0ef97f2d27e4234936b7ad85c3413104f120cbba
SaidHirzi <159710980+SaidHirzi@users.noreply.github.com>
2024-03-14 04:31:06 +0700

    Add files via upload

---
+++ Sosmed/src/js/index.js
@@ -0,0 +1,19 @@
+/*? no js js needed from me */
+
+
+function instagram(){
+    console.log("test")
+    location.assign("https://www.instagram.com/said.hirzi/?hl=en")
+}
+function twitter(){
+    console.log("test")
+    location.assign("https://twitter.com/Sai_it")
+}
+function youtube(){
+    console.log("test")
+    location.assign("https://www.youtube.com/channel/UCq0YwI-q1a-PcMkBuBPWUOQ")
+}
+function tiktok(){
+    console.log("test")
+    location.assign("https://www.tiktok.com/@sipdek123")
+}
